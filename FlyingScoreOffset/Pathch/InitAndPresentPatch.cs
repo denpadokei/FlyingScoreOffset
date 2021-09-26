f7e715228b636d677f5169fe8c1c61e946e07b44
デンパ時計 <jsoldenpa@yahoo.co.jp>
2021-09-27 01:35:16 +0900

    プロジェクト ファイルを追加します。

---
+++ FlyingScoreOffset/Pathch/InitAndPresentPatch.cs
@@ -0,0 +1,22 @@
+﻿using FlyingScoreOffset.Configuration;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FlyingScoreOffset.Pathch
+{
+    [HarmonyPatch(typeof(FlyingObjectEffect), nameof(FlyingObjectEffect.InitAndPresent),
+        new Type[] { typeof(float), typeof(Vector3), typeof(Quaternion), typeof(bool) })]
+    public class InitAndPresentPatch
+    {
+        public static void Prefix(ref float duration, ref Vector3 targetPos, ref Quaternion rotation, ref bool shake)
+        {
+            var newPos = new Vector3(targetPos.x + PluginConfig.Instance.Offset.x, targetPos.y + PluginConfig.Instance.Offset.y, targetPos.z + PluginConfig.Instance.Offset.z);
+            targetPos = newPos;
+        }
+    }
+}
---
+++ FlyingScoreOffset/Plugin.cs
@@ -0,0 +1,64 @@
+﻿using HarmonyLib;
+using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using IPALogger = IPA.Logging.Logger;
+
+namespace FlyingScoreOffset
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+        public const string HARMONY_ID = "FlyingScoreOffset.denpadokei.github.com";
+        private Harmony harmony;
+        [Init]
+        /// <summary>
+        /// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
+        /// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
+        /// Only use [Init] with one Constructor.
+        /// </summary>
+        public void Init(IPALogger logger, Config conf)
+        {
+            Instance = this;
+            Log = logger;
+            Log.Debug("Config loaded");
+            Log.Info("FlyingScoreOffset initialized.");
+            Configuration.PluginConfig.Instance = conf.Generated<Configuration.PluginConfig>();
+            this.harmony = new Harmony(HARMONY_ID);
+        }
+
+        [OnStart]
+        public void OnApplicationStart()
+        {
+            Log.Debug("OnApplicationStart");
+        }
+
+        [OnExit]
+        public void OnApplicationQuit()
+        {
+            Log.Debug("OnApplicationQuit");
+
+        }
+
+        [OnEnable]
+        public void OnEnable()
+        {
+            this.harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+            this.harmony.UnpatchAll(HARMONY_ID);
+        }
+    }
+}
